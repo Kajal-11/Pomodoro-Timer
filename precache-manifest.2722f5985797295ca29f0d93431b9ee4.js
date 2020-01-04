6fcb9742797ba911e1590ba04a820fe9472e112b
Kajal <kajal11122000@gmail.com>
2020-01-04 12:51:04 +0530

    Updates

---
+++ precache-manifest.2722f5985797295ca29f0d93431b9ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ce78ff5aecc667322918933a0b76cf",
+    "url": "/pomodoroTimer/index.html"
+  },
+  {
+    "revision": "0e3b07fb497cc3a9d9c5",
+    "url": "/pomodoroTimer/static/css/main.75076530.chunk.css"
+  },
+  {
+    "revision": "c75a301b1f6728fdc7f8",
+    "url": "/pomodoroTimer/static/js/2.d417d38c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pomodoroTimer/static/js/2.d417d38c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e3b07fb497cc3a9d9c5",
+    "url": "/pomodoroTimer/static/js/main.5698f4fc.chunk.js"
+  },
+  {
+    "revision": "3b26cd7bc2bcfedc963b",
+    "url": "/pomodoroTimer/static/js/runtime-main.dd0c6c39.js"
+  }
+]);
